ca6b4c083ba6feb8d48ad814fe17b0382ce0cca6
Álvaro Lillo Igualada <alilloig@gmail.com>
2025-04-14 11:23:19 +0200

    Scafolding

---
+++ evm/test/MMR.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MMR} from "../src/MMR.sol";
+
+contract MMRTest is Test {
+    Counter public counter;
+
+    function setUp() public {
+
+}
